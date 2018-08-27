a3c1dc8304cfe3712b216ed899931ae6f18ff40f
Alex Shvid <a@shvid.com>
2018-08-27 16:22:32 -0700

    implement BiStream

--- src/main/java/rocks/gkvs/BiStream.java
+++ src/main/java/rocks/gkvs/BiStream.java
@@ -1,5 +1,16 @@
 package rocks.gkvs;
 
+import java.util.function.Consumer;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import rx.Observable;
+import rx.Observable.OnSubscribe;
+
+
 /**
  * 
  * BiStream
@@ -30,5 +41,119 @@ public abstract class BiStream<S, R> {
 	 */
 	
 	public abstract Observer<S> async(Observer<R> observer);
+	
+	
+	public Observable<R> observe(final Observable<S> incoming) {
+		
+		return Observable.unsafeCreate(new OnSubscribe<R>() {
+
+			@Override
+			public void call(final rx.Subscriber<? super R> subscriber) {
+
+				final Observer<S> sender = async(new Observer<R>() {
+
+					@Override
+					public void onNext(R item) {
+						subscriber.onNext(item);
+					}
+
+					@Override
+					public void onError(Throwable t) {
+						subscriber.onError(t);
+					}
+
+					@Override
+					public void onCompleted() {
+						subscriber.onCompleted();
+					}
+					
+				});
+				
+				incoming.subscribe(new rx.Observer<S>() {
+
+					@Override
+					public void onNext(S item) {
+						sender.onNext(item);
+					}
+
+					@Override
+					public void onError(Throwable t) {
+						sender.onError(t);
+					}
+
+					@Override
+					public void onCompleted() {
+						sender.onCompleted();
+					}
+					
+				});
+				
+			}
+			
+		});
+		
+	}
+	
+	
+	/**
+	 * Creates out-coming flux based on incoming one
+	 * 
+	 * @param incoming - flux
+	 * @return output stream
+	 */
+	
+	public Flux<R> flux(final Flux<S> incoming) {
+		
+		return Flux.create(new Consumer<FluxSink<R>>() {
+
+			@Override
+			public void accept(final FluxSink<R> sink) {
+
+				final Observer<S> sender = async(new Observer<R>() {
+
+					@Override
+					public void onNext(R item) {
+						sink.next(item);
+					}
+
+					@Override
+					public void onError(Throwable t) {
+						sink.error(t);
+					}
+
+					@Override
+					public void onCompleted() {
+						sink.complete();
+					}
+					
+				});
+
+				incoming.subscribe(new Subscriber<S>() {
+
+					@Override
+					public void onSubscribe(Subscription s) {
+					}
+
+					@Override
+					public void onNext(S item) {
+						sender.onNext(item);
+					}
+
+					@Override
+					public void onError(Throwable t) {
+						sender.onError(t);
+					}
+
+					@Override
+					public void onComplete() {
+						sender.onCompleted();
+					}
+					
+				});
+
+			}
+			
+		});
+	}
 	
 }
--- src/main/java/rocks/gkvs/IncomingStream.java
+++ src/main/java/rocks/gkvs/IncomingStream.java
@@ -1,6 +1,13 @@
 package rocks.gkvs;
 
 import java.util.Iterator;
+import java.util.function.Consumer;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import rx.Observable;
+import rx.Observable.OnSubscribe;
+import rx.Subscriber;
 
 /**
  * 
@@ -31,5 +38,82 @@ public abstract class IncomingStream<R> {
 	 */
 	
 	public abstract void async(Observer<R> observer);
+	
+	/**
+	 * Creates observable stream (RX-JAVA)
+	 * 
+	 * @return stream
+	 */
+	
+	public Observable<R> observe() {
+		
+		return Observable.unsafeCreate(new OnSubscribe<R>() {
+
+			@Override
+			public void call(final Subscriber<? super R> subscriber) {
+
+				async(new Observer<R>() {
+
+					@Override
+					public void onNext(R item) {
+						subscriber.onNext(item);
+					}
+
+					@Override
+					public void onError(Throwable t) {
+						subscriber.onError(t);
+					}
+
+					@Override
+					public void onCompleted() {
+						subscriber.onCompleted();
+					}
+					
+				});
+				
+			}
+			
+		});
+		
+	}
+	
+	
+	/**
+	 * Creates Flux stream for the result (REACTOR)
+	 * 
+	 * @return flux
+	 */
+	
+	public Flux<R> flux() {
+		
+		return Flux.create(new Consumer<FluxSink<R>>() {
+
+			@Override
+			public void accept(final FluxSink<R> sink) {
+
+				async(new Observer<R>() {
+
+					@Override
+					public void onNext(R item) {
+						sink.next(item);
+					}
+
+					@Override
+					public void onError(Throwable t) {
+						sink.error(t);
+					}
+
+					@Override
+					public void onCompleted() {
+						sink.complete();
+					}
+					
+				});
+				
+			}
+			
+		});
+		
+	}
 	
 }
